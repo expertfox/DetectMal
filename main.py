b350715cbeb394598149eff167a5a71ecc7550b9
Rajith Bandara <rajithpriv@gmail.com>
2021-03-17 14:29:51 +0530

    commit
    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+import re
+import os
+
+MalWord = "MALWARE"
+
+with open('requestList.txt') as f:
+    requests = f.readlines()
+
+    for req in requests:
+        if req.find(MalWord):
+            IPADRRESS = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', req).group()
+
+            os.system('iptables -A INPUT -s '+ IPADRRESS + '-p tcp --destination -port 80 -j DROP')
+            os.system('iptables -A INPUT -s' + IPADRRESS + '-p tcp --destination -port 443 -j DROP')
