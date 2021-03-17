4845cb3ec8fc8229cc5631fe7ad13dc7fba57d49
Rajith Bandara <rajithpriv@gmail.com>
2021-03-17 14:43:46 +0530

    this is with the reqquest list text file
    second commit

--- main.py
+++ main.py
@@ -11,5 +11,7 @@ with open('requestList.txt') as f:
         if req.find(MalWord):
             IPADRRESS = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', req).group()
 
+            print(IPADRRESS)
+
             os.system('iptables -A INPUT -s '+ IPADRRESS + '-p tcp --destination -port 80 -j DROP')
             os.system('iptables -A INPUT -s' + IPADRRESS + '-p tcp --destination -port 443 -j DROP')
